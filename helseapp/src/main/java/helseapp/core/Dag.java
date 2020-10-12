3552c5c6b70a18ba58e36bd15bb7cc75175e881c
David Ramsvik Andersen <davidra@stud.ntnu.no>
2020-10-12 00:52:26 +0000

    Lagt til javadoc-kommentarer på metoder i Dag, Grafmetoder og Dager

--- helseapp/src/main/java/helseapp/core/Dag.java
+++ helseapp/src/main/java/helseapp/core/Dag.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 public class Dag{
 
+    //Definerer alle variablene dag objektet trenger
+
     private double vekt;
     private double skritt;
     private double treningstid;
@@ -11,6 +13,17 @@ public class Dag{
     private double karbo;
     private double fett;
     private LocalDate date;
+
+    /**
+	* klasse konstruktør
+	* @param vekt - vekten til brukeren
+	* @param skritt - antall skritt
+	* @param treningstid - tid brukt til trening
+	* @param protein - mengde protein inntatt
+	* @param karbo - mengde karbohydrater inntatt
+	* @param fett - mengde fett inntatt
+	* @param date - dato for dagen
+	*/
 
     public Dag(double vekt, double skritt, double treningstid, double protein, double karbo, double fett, LocalDate date) {
         this.vekt = vekt;
@@ -21,30 +34,65 @@ public class Dag{
         this.fett = fett;
         this.date = date;
     }
+    
+	/**
+	* henter vekten til brukeren
+	* @return vekt - brukers vekt
+	*/
 
     public double getVekt() {
         return vekt;
     }
 
+    /**
+	* henter treningstiden til brukeren
+	* @return treningstid - brukers treningstid
+	*/
+
     public double getTreningstid() {
         return treningstid;
     }
+
+	/**
+	* henter proteininntaket til brukeren
+	* @return protein - mengde protein inntatt
+	*/
 
     public double getProtein() {
         return protein;
     }
 
+	/**
+	* henter karbohydratinntaket til brukeren
+	* @return karbo - mengde karbohydrater inntatt
+	*/
+
     public double getKarbo() {
         return karbo;
     }
+
+	/**
+	* henter fettinntaket til brukeren
+	* @return fett - mengde fett inntatt
+	*/
 
     public double getFett() {
         return fett;
     }
 
+	/**
+	* henter antall skritt til brukeren
+	* @return skritt - antall skritt
+	*/
+
     public double getSkritt() {
         return skritt;
     }
+
+	/**
+	* henter datoen
+	* @return date - datoen for dagen
+	*/
 
     public LocalDate getDate() { return date; }
 }
--- helseapp/src/main/java/helseapp/core/Grafmetoder.java
+++ helseapp/src/main/java/helseapp/core/Grafmetoder.java
@@ -4,6 +4,14 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 
 public class Grafmetoder {
+
+    /**
+	* metode for å lage en graf og plotte inn data i den, med navn på grafen
+	* @param  data - data som hentes fra en todimensjonal liste med x og y verdier som brukes i charten
+	* @param chart - kobler charten mot fxml chart som skal brukes for den spesifikke charten
+	* @param chartName - navnet på grafen
+    */
+    
     public static void leggDataIGraf(Double[][] data, LineChart<String, Number> chart, String chartName) {
         XYChart.Series<String, Number> series = new XYChart.Series<>();
         series.setName(chartName);
