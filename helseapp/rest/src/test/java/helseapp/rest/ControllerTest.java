0a47ae8826381a0cf20ee54eae66c55d989bcb10
Kristine Larssen <krlarss@stud.ntnu.no>
2020-11-19 15:18:08 +0000

    #25 har økt dekningsgraden til 76prosent. mangler å teste postmapping

--- helseapp/rest/src/test/java/helseapp/rest/ControllerTest.java
+++ helseapp/rest/src/test/java/helseapp/rest/ControllerTest.java
@@ -29,6 +29,7 @@ import java.util.Arrays;
 import java.util.List;
 
 
+
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = { Controller.class, Helseapplication.class })
 @WebMvcTest
@@ -37,21 +38,35 @@ public class ControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  private ObjectMapper objectMapper = new ObjectMapper();
+  private ObjectMapper objectMapper;
+
 
   @BeforeEach
   public void setup() throws Exception {
     objectMapper = new ObjectMapper().registerModule(new DagerModule());;
   }
 
+
   @Test
-  public void testGet_dag() throws Exception {
+  public void testGet_home() throws Exception {
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/")
+        .accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andReturn();
+    String innhold = result.getResponse().getContentAsString();
+    assertEquals("hello", innhold);
+  }
+
+
+  @Test
+  public void testGet_dager() throws Exception {
     MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/dager/")
         .accept(MediaType.APPLICATION_JSON))
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andReturn();
     try {
         Dager[] dager = objectMapper.readValue(result.getResponse().getContentAsString(), Dager[].class);
+        System.out.println(dager[0]);
         List<Dag> dager2 = new ArrayList(Arrays.asList(dager));
         Iterator<Dag> it = dager2.iterator();
         assertTrue(it.hasNext());
@@ -63,24 +78,25 @@ public class ControllerTest {
         assertTrue(it.hasNext());
         Dag dag4 = it.next();
         assertFalse(it.hasNext());
-        System.out.println(LocalDate.parse("2020-11-11"));
-        System.out.println(dag1);
-        //assertEquals(LocalDate.parse("2020-11-11"), dag1.getDate());
     } catch (JsonProcessingException e) {
         fail(e.getMessage());
     }
-
   }
 
 
-  @Test
-  public void testGet_todo2() throws Exception {
-    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/")
-        .accept(MediaType.APPLICATION_JSON))
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andReturn();
+    @Test
+    public void testGet_ByDato() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/dager/2020-11-10")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn();
+        String innhold = result.getResponse().getContentAsString();
+        String forventet_innhold = "{\"vekt\":10.0,\"skritt\":20.0,\"treningstid\":30.0,\"protein\":40.0,\"karbo\":50.0,\"fett\":60.0,\"date\":\"2020-11-10\",\"kalorier\":900.0}";
+        assertEquals(forventet_innhold, innhold);
+    }
 
-  }
+    
+
 
 
 
